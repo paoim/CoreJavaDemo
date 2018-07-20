981487dd992d072482f8b833dc82194f0c695172
Pao Im <paoim@yahoo.com>
2018-07-19 22:59:14 -0500

    Add Synchronized block and method demo

---
+++ src/com/paoim/coredemo/synchronization/before/Line.java
@@ -0,0 +1,19 @@
+package com.paoim.coredemo.synchronization.before;
+
+public class Line {
+	
+	// if multiple threads(trains) will try to
+	// access this unsynchronized method,
+	// they all will get it. So there is chance
+	// that Object's  state will be corrupted.
+	public void printLine() {
+		for (int i =0; i < 3; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(400);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
---
+++ src/com/paoim/coredemo/synchronization/before/UnsynchronizedDemo.java
@@ -0,0 +1,19 @@
+package com.paoim.coredemo.synchronization.before;
+
+public class UnsynchronizedDemo {
+	
+	public static void run() {
+		// Object of Line class that is shared
+		// among the threads.
+		Line obj = new Line();
+
+		// creating the threads that are
+		// sharing the same Object.
+		Train train1 = new Train(obj);
+		Train train2 = new Train(obj);
+
+		// threads start their execution.
+		train1.start();
+		train2.start();
+	}
+}
